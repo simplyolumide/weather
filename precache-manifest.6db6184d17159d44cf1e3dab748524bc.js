698f1b6cfaee601235bd1570cd4175be7571b6b6
graphics-editor <graphics-editor@graphics-editors-iMac.local>
2019-12-28 03:08:10 +0100

    Updates

---
+++ precache-manifest.6db6184d17159d44cf1e3dab748524bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a760bad54bd971898c15eef086e2961",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "0c12b137da9127ef1417",
+    "url": "/weather/static/css/main.9a682b11.chunk.css"
+  },
+  {
+    "revision": "692b193a8065d9c3cd59",
+    "url": "/weather/static/js/2.99967b50.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/weather/static/js/2.99967b50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c12b137da9127ef1417",
+    "url": "/weather/static/js/main.cfce1fde.chunk.js"
+  },
+  {
+    "revision": "36ddb3f7f3e7d04b8c9c",
+    "url": "/weather/static/js/runtime-main.24985744.js"
+  }
+]);
