a7492ed2a1330a6f6a3a499f535e97bf36144d26
simplyolumide <olumide_adewusi@yahoo.com>
2021-04-19 13:11:12 -0700

    build push

---
+++ build/precache-manifest.b30564aa7840c76a3b36103da03eb058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317f6627627ed0aa22f58ec2b006dd88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fea52d71e6f702d94606",
+    "url": "/static/css/main.58ab9f77.chunk.css"
+  },
+  {
+    "revision": "692b193a8065d9c3cd59",
+    "url": "/static/js/2.99967b50.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/static/js/2.99967b50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fea52d71e6f702d94606",
+    "url": "/static/js/main.76c4d749.chunk.js"
+  },
+  {
+    "revision": "507de9b71e25a75f3a45",
+    "url": "/static/js/runtime-main.7b6df24c.js"
+  }
+]);
